bcc3643c86012cd62502f221d4e140fdd20e7fea
linus kronstrom <linus.kronstrom@gmail.com>
2022-01-10 23:12:14 +0100

    just messing with files

--- AdventOfCode/Day4/Day4.cs
+++ AdventOfCode/Day4/Day4.cs
@@ -4,7 +4,7 @@ public static class Day4
 {
     public static void A1()
     {
-        var lines = File.ReadLines("C:\\Users\\linus\\RiderProjects\\AdventOfCode\\AdventOfCode\\Day4\\info.txt").ToList();
+        var lines = File.ReadLines("/home/linus/RiderProjects/AdventOfCode/AdventOfCode/Day4/info.txt").ToList();
 
         var bingoNumbers = lines[0].Split(",").Select(x => Convert.ToInt32(x)).ToList();
 
@@ -12,11 +12,12 @@ public static class Day4
 
         var newLines = lines.Select(x => x.Split(" ", StringSplitOptions.RemoveEmptyEntries)).ToList();
 
-        var intLines = lines.Select(x => Convert.ToInt32(x)).ToList();
-
-        foreach (var line in intLines)
+        foreach (var line in newLines)
         {
-            Console.WriteLine(line);
+            foreach (var myVar in line)
+            {
+                Console.WriteLine(myVar);
+            }
         }
     }
 }
